a0f7909af4fe9c000335a9816be06682850ce523
SherlonAlmeida <sherlonalmeidadasilva@gmail.com>
2023-10-14 17:27:40 -0300

    Primeiro app

--- hello_django/hello_django/urls.py
+++ hello_django/hello_django/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for projeto_hospital project.
+URL configuration for hello_django project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from projeto_hospital.views import hello
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', hello, name='hello'),
+    path('hello/', views.hello)
 ]
--- hello_django/hello_django/wsgi.py
+++ hello_django/hello_django/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for projeto_hospital project.
+WSGI config for hello_django project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projeto_hospital.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hello_django.settings')
 
 application = get_wsgi_application()
