b2344355e3f1ede34b5e04dacfa514966a9442e8
SherlonAlmeida <sherlonalmeidadasilva@gmail.com>
2023-10-14 19:58:38 -0300

    Criacao de banco de dados usando o arquivo models.py

--- agenda/core/admin.py
+++ agenda/core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from core.models import Evento #Sherlon: Adicionado
 
 # Register your models here.
+
+class EventoAdmin(admin.ModelAdmin):
+    #Mostra os CAMPOS desejados na rota 'admin/'
+    list_display = ('titulo', 'data_evento', 'data_criacao')
+
+    #Apresenta um filtro interativo pelos CAMPOS definidos
+    list_filter = ('usuario', 'data_evento', )
+
+admin.site.register(Evento, EventoAdmin) #Sherlon: Adicionado
--- agenda/core/models.py
+++ agenda/core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User #Sherlon: importa a tabela default "User"
 
 # Create your models here.
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    data_evento = models.DateTimeField(verbose_name="Data do Evento")
+    data_criacao = models.DateTimeField(auto_now=True) #Insere a hora atual neste campo
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE) #Sherlon: models.CASCADE é o comportamento a ser definido caso um usuário for definido do banco, neste caso também é excluído todos os eventos deste usuário.
+
+    class Meta: #Força que o nome da tabela seja "evento" e não "core_evento"
+        db_table = 'evento'
+    
+    def __str__(self):
+        return self.titulo
